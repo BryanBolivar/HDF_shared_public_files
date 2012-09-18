332b7ecc2bbfa62293aa56de5e837fe8c535b407
Rene Santis <rene.santis@koombea.com>
2012-09-18 10:48:30 -0500

    Fixed next prev buttons, Month button, shadowling for calendars numbers

--- js/mobileapp.js
+++ js/mobileapp.js
@@ -183,6 +183,7 @@ function insert_city_events( )
     } else {
 	showoptions( );
     }
+    
 }
 
 //----------------------------------------------------------------------
@@ -583,14 +584,14 @@ function make_grid_cal( )
 	html_str += "<td>";
 
 	if( month_cnt > 0 ) {   // No left arrow if this is 1st month
-	    html_str += "<img border=0 onclick='set_date_to_first_day_of_this_month(" + (month_cnt-1) + ");' src='/public/sharedhtml/images/arrow_left.png'>";
+	    html_str += "<a href='javascript:void(0)' onclick='set_date_to_first_day_of_this_month(" + (month_cnt-1) + ");' style='padding: 0 10px;'><img border=0 src='/public/sharedhtml/images/arrow_left.png'></a>";
 	}
 
 	html_str +="</td>\n<td colspan=5>" + month_names[ current_month.getMonth( ) ]
 	    + " " + current_month.getFullYear( ) + "</td>\n<td>\n";
 
 	if( month_cnt < 2 ) { // No right arrow if this is last month
-	    html_str += "<img border=0 onclick='set_date_to_first_day_of_this_month(" + (month_cnt+1) + ");' src='/public/sharedhtml/images/arrow_right.png'>";
+	    html_str += "<a href='javascript:void(0)' onclick='set_date_to_first_day_of_this_month(" + (month_cnt+1) + ");' style='padding: 0 10px;'><img border=0 src='/public/sharedhtml/images/arrow_right.png'></a>";
 	}
 
 	//--------------------------------------------------------------------
